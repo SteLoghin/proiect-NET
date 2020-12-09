6b9b14c092c36921a0af1670877ab912aa660eb7
RazTutu <raztutu123@gmail.com>
2020-12-09 23:33:57 +0200

    changed architecture to react

---
+++ react_frontend_test/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import PostForm from './Components/PostForm';
+
+function App() {
+  return (
+    <React.Fragment>
+    <h1>DotNetHouse</h1>
+    <PostForm />
+    </React.Fragment>
+  );
+}
+
+export default App;
