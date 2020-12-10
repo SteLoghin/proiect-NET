9f2eca6251abb076dd999b144764fc7bcb8dbc15
Stelian Loghin <alexandruloghinstelian121@yahoo.com>
2020-12-10 22:56:50 +0200

    Improved design

--- react_frontend_test/src/Pages/MainNavigation.js
+++ react_frontend_test/src/Pages/MainNavigation.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import '../Style/mainNavigation.css';
+
 
 const MainNavigation = props => {
     return (
         <header className="main-navigation">
             <div className="main-navigation__logo">
-                <h1>DotNetHouse Menu</h1>
+                <h1>DotNetHouse</h1>
             </div>
             
             <nav className="main-navigation__item">
                 <ul>
                     <React.Fragment>
                         <li><NavLink to="/home">Home</NavLink></li>
-                        <li><NavLink to="/price-prediction">Price Prediction Form</NavLink></li>
+                        <li><NavLink to="/price-prediction">PricePredictionForm</NavLink></li>
                         <li><NavLink to="/endpoint2">TestEndpoint2</NavLink></li>
                         <li><NavLink to="/endpoint3">TestEndpoint3</NavLink></li>
                     </React.Fragment>
--- react_frontend_test/src/Pages/PostForm.js
+++ react_frontend_test/src/Pages/PostForm.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from 'react';
+import axios from 'axios';
+import '../Style/postForm.css';
 class PostForm extends Component {
 	constructor(props) {
 		super(props)
@@ -41,92 +42,106 @@ class PostForm extends Component {
 	render() {
 		const { lotArea,lotConfig,zone,floors,overallCond,yearBuilt,rooms,saleCondition,salePrice } = this.state
 		return (
-			<div>
+			<div className="dataForm">
 				<form onSubmit={this.submitHandler}>
+					<h2>Spune-ne ce locuinta iti doresti</h2>
+					<h3>Noi iti aratam pretul :)</h3>
 					<div>
+						<label for="lotArea">Lot Area</label><br />
 						<input
 							type="number"
-                            name="lotArea"
-                            placeholder="lotArea"
+							name="lotArea"
+							id="lotArea"
+                            placeholder="Lot Area"
 							value={lotArea}
 							onChange={this.changeHandler}
+							
 						/>
 					</div>
 					<div>
+					<label for="lotConfig">Lot Config</label><br />
 						<input
 							type="text"
                             name="lotConfig"
-                            placeholder="lotConfig"
 							value={lotConfig}
 							onChange={this.changeHandler}
+							id="lotConfig"
 						/>
 					</div>
 					<div>
+					<label for="zone">Zone</label><br />
 						<input
 							type="text"
-                            name="zone"
-                            placeholder="zone"
+							name="zone"
+							id="zone"
 							value={zone}
 							onChange={this.changeHandler}
 						/>
 					</div>
 					<div>
+					<label for="floors">Floors</label><br />
 						<input
 							type="text"
                             name="floors"
-                            placeholder="floors"
 							value={floors}
 							onChange={this.changeHandler}
+							id="floors"
 						/>
 					</div>
 					<div>
+					<label for="overallCond">Overall Condition</label><br />
 						<input
 							type="number"
                             name="overallCond"
-                            placeholder="overallCond"
+                            id="overallCond"
 							value={overallCond}
 							onChange={this.changeHandler}
 						/>
 					</div>
 					<div>
+					<label for="yearBuilt">Year Built</label><br />
 						<input
 							type="number"
                             name="yearBuilt"
-                            placeholder="yearBuilt"
+                            id="yearBuilt"
 							value={yearBuilt}
 							onChange={this.changeHandler}
 						/>
 					</div>
 					<div>
+					<label for="rooms">Rooms</label><br />
 						<input
 							type="number"
                             name="rooms"
-                            placeholder="rooms"
+                            id="rooms"
 							value={rooms}
 							onChange={this.changeHandler}
 						/>
 					</div>
 					<div>
+					<label for="saleCondition">Sale Condition</label><br />
 						<input
 							type="text"
                             name="saleCondition"
-                            placeholder="saleCondition"
+                            id="saleCondition"
 							value={saleCondition}
 							onChange={this.changeHandler}
 						/>
 					</div>
 					<div>
+					<label for="salePrice">Sale Price</label><br />
 						<input
 							type="number"
                             name="salePrice"
-                            placeholder="salePrice"
+                            id="salePrice"
 							value={salePrice}
 							onChange={this.changeHandler}
 						/>
 					</div>
-					<button type="submit">Predict</button>
+					<button type="submit">Trimite</button>
+					<p id="raspuns"></p>
 				</form>
-				<p id="raspuns"></p>
+				
 			</div>
 		)
 	}
