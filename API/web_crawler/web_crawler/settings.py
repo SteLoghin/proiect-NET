86a0e6854896dff9a7bd7735da71d83de92dd3d7
Damian Alexandru <damian.alexandru99@gmail.com>
2021-01-06 00:21:23 +0200

    added heroku configuration

--- API/web_crawler/web_crawler/settings.py
+++ API/web_crawler/web_crawler/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -135,3 +135,4 @@ CORS_ORIGIN_ALLOW_ALL = True # development, in production this should be false, 
 #   'http://localhost:8000',
 # )
 
+django_heroku.settings(locals())
