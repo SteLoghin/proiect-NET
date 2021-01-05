0072c63137ae9c394348b1ed13dff39d4fd90fa5
Damian Alexandru <damian.alexandru99@gmail.com>
2021-01-05 12:35:06 +0200

    added crawler to a website and api endpoint for rpc, and inserts in mongodb

--- API/web_crawler/api/models.py
+++ API/web_crawler/api/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 
+class PropertyManager(models.Manager):
+    def create_property(self, dict):
+        new_property = self.create(rooms=dict["rooms"], 
+                                area=dict["area"], 
+                                floor=dict["floor"], 
+                                year=dict["year"], 
+                                bathrooms=dict["bathrooms"], 
+                                kitchens=dict["kitchens"], 
+                                link=dict["link"], 
+                                zone=dict["zone"], 
+                                price=dict["price"])
+        return new_property
+
 class Property(models.Model):
     # TODO: add a timestamp for db entries
     rooms = models.IntegerField()
@@ -11,5 +24,6 @@ class Property(models.Model):
     link = models.URLField(blank=True)
     zone = models.CharField(max_length=255)
     price = models.FloatField()
+    objects = PropertyManager()
 
     
--- API/web_crawler/api/views.py
+++ API/web_crawler/api/views.py
@@ -4,9 +4,12 @@ from rest_framework.decorators import api_view
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import viewsets
+import requests
+from bs4 import BeautifulSoup
+import re, time
 
 from .serializers import PropertySerializer
-from .models import Property
+from .models import Property, PropertyManager
 
 class PropertyViewSet(viewsets.ModelViewSet):
     queryset = Property.objects.all()
@@ -16,8 +19,105 @@ class PropertyViewSet(viewsets.ModelViewSet):
 @method_decorator(csrf_exempt, name='dispatch')
 class Crawler(View):
     def post(self, request):
-        property_instance = Property.objects.create(rooms=1, area=50, floor=5, year=2000, bathrooms=2, kitchens=1, link="testlink", zone="Iasi", price=200)
-        if property_instance is None:
-            return HttpResponse(content="Failed to collect data", status=204)
-        else:
-            return HttpResponse(content="Collected data", status=200)
+        #TODO: decide if we should delete them or soft delete them and keep them
+
+        crawl_results = {}
+        try:
+            Property.objects.all().delete()
+            start_time_s1 = time.time() 
+            properties = self.crawl_titirez()
+            crawl_results["titrez.ro"] = {
+                "total_crawl_time": time.time() - start_time_s1,
+                "length": len(properties)
+            }
+            for p in properties:
+                p.save()
+        except Exception as e:
+            crawl_results["titirez.ro"] = {"error": str(e.with_traceback)}
+            return JsonResponse(crawl_results, status=500)
+        
+        return JsonResponse(crawl_results, status=200)
+    
+    def crawl_titirez(self):
+        properties = list()
+        total = requests.get('https://www.titirez.ro/inchirieri-apartamente/iasi')
+        total_soup = BeautifulSoup(total.content, 'html.parser')
+        nr_iteratii = total_soup.find('a', class_='page-nav mobile-no', attrs={'title': 'Total pagini'})
+        nr_iteratii = re.search('[0-9]+', str(nr_iteratii)).group(0)
+        for i in range(0, int(nr_iteratii)):
+            page = requests.get('https://www.titirez.ro/inchirieri-apartamente/iasi?p=' + str(i))
+            page_soup = BeautifulSoup(page.content, 'html.parser')
+            links = page_soup.find_all('a', attrs={'itemprop': 'url'}, class_='item-btn item-btn-default')
+            links = list(map(lambda x: x['href'], links))
+            row_list = []
+            final_list = []
+            
+            for k in links:
+                page2 = requests.get(k)
+                page2_soup = BeautifulSoup(page2.content, 'html.parser')
+                aux = page2_soup.find_all('span', class_='value')
+                pret = page2_soup.find_all('div', class_='product-price')
+                zona = page2_soup.find_all(class_='sidebar-title')
+                t = 0
+                j = 0
+                cauta_pret = 0
+                cauta_zona = ''
+                for i in range(0, len(aux)):
+                    if j < len(pret):
+                        cauta_pret = re.search('[1-9][0-9]*', str(pret[j]))
+                        j = j + 1
+                        if cauta_pret:
+                            cauta_pret = cauta_pret.group(0)
+                    if t < len(zona):
+                        cauta_zona = re.search('Zona [A-Za-z ]+', str(zona[t]))
+                        if cauta_zona:
+                            cauta_zona = cauta_zona.group(0)
+                        t = t + 1
+                    if i == 0:
+                        nr_camere = re.search('[0-9]+', str(aux[i]))
+                    if i == 1:
+                        suprafata = re.search('[0-9]+', str(aux[i]))
+                    if i == 3:
+                        etaj = re.search('[0-9]+', str(aux[i]))
+                        if etaj:
+                            etaj = etaj.group(0)
+                        else:
+                            etaj = 0
+                    if i == 4:
+                        an_constructie = re.search('[0-9]+', str(aux[i]))
+                        if an_constructie:
+                            an_constructie = an_constructie.group(0)
+                        else:
+                            an_constructie = 0
+                    if i == 7:
+                        nr_bai = re.search('[0-9]+', str(aux[i]))
+                        if nr_bai:
+                            nr_bai = nr_bai.group(0)
+                        else:
+                            nr_bai = 0
+                    if i == 8:
+                        nr_bucatarii = re.search('-?[0-9]+', str(aux[i]))
+                        if nr_bucatarii:
+                            nr_bucatarii = nr_bucatarii.group(0)
+                        else:
+                            nr_bucatarii = 0
+                dictionar = {
+                    "rooms": nr_camere.group(0),
+                    "area": suprafata.group(0),
+                    "floor": etaj,
+                    "year": an_constructie,
+                    "bathrooms": nr_bai,
+                    "kitchens": nr_bucatarii,
+                    "link": k,
+                    "zone": cauta_zona,
+                    "price": cauta_pret,
+                }
+                if dictionar['year'] != 0:
+                    # adauga proprietatea la o lista
+                    properties.append(Property.objects.create_property(dict=dictionar))
+
+                del row_list[:]
+                del aux[:]
+                dictionar.clear()
+
+        return properties
