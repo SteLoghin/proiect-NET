622c1808514d9ba1b5dce498eb4d45db53a1ee9b
Stelian Loghin <alexandruloghinstelian121@yahoo.com>
2021-01-14 10:39:53 +0200

    modified admin loghin

--- react_frontend_test/src/Pages/Admin.js
+++ react_frontend_test/src/Pages/Admin.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import '../Style/AdminLogin.css';
 import axios from 'axios';
+require('dotenv').config('../../.env');
+console.log(process.env)
+// console.log(process.env)
 // import { NavLink } from 'react-router-dom';
 
 
@@ -26,21 +29,28 @@ class AdminPage extends Component {
     submitHandler = e => {
 		e.preventDefault()
 		console.log(this.state)
-		axios
-		.get('http://localhost:5000/api/v1/predictions')
-			// .get('http://dotnethouse.azurewebsites.net/api/v1/predictions',this.state)
-			.then(response => {
-				console.log(response.status)
-				const data=JSON.parse(JSON.stringify(response))
-				console.log(data)
-                if(response.status === 200){
-                    console.log("here");
-                    this.props.history.push("/admin-dashboard")
-                }
-			})
-			.catch(error => {
-				console.log(error)
-			})
+		const name=document.getElementById("login").value
+		const password=document.getElementById("password").value
+		if(name===process.env.REACT_APP_ADMIN_NAME && password===process.env.REACT_APP_ADMIN_PASSWORD){
+			this.props.history.push("/admin-dashboard")
+		}else{
+			alert("Invalid username or password")
+		}
+		// axios
+		// .get('http://localhost:5000/api/v1/predictions')
+		// 	// .get('http://dotnethouse.azurewebsites.net/api/v1/predictions',this.state)
+		// 	.then(response => {
+		// 		console.log(response.status)
+		// 		const data=JSON.parse(JSON.stringify(response))
+		// 		console.log(data)
+        //         if(response.status === 200){
+        //             console.log("here");
+        //             this.props.history.push("/admin-dashboard")
+        //         }
+		// 	})
+		// 	.catch(error => {
+		// 		console.log(error)
+		// 	})
 	}
 
 
@@ -56,7 +66,7 @@ class AdminPage extends Component {
                 </div>
 
                 <form onSubmit={this.submitHandler}>
-                    <input type="text" id="login" className="inpt-field" name="login" placeholder="login" />
+                    <input type="text" id="login" className="inpt-field" name="login" placeholder="name" />
                     <input type="password" id="password" className="inpt-field" name="login" placeholder="password" />
                     <input type="submit" className="inpt-field inpt-button" value="LogIn"/>
                 </form>
